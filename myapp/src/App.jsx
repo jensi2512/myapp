5bb13d0215e49939de32e5e4b930bdfe6c8ebe12
jensi2512 <jensipatel19@gnu.ac.in>
2025-03-05 10:37:28 +0530

    update

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,6 +1,17 @@
-function App(){
+import Home from "./Component/Home"
+import About from "./Component/About"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function App() {
   return <>
-  <h1>hello</h1></>
+    <h1>hello</h1>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 }
 
 export default App
