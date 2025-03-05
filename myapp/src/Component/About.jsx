8bbc7b81bb5757211adca178298678e96296009a
jensi2512 <jensipatel19@gnu.ac.in>
2025-03-05 19:54:58 +0530

    update

--- myapp/src/Component/About.jsx
+++ myapp/src/Component/About.jsx
@@ -1,7 +1,78 @@
-function About(){
+import '../static/About.css'
+import image from '../img/Analysis.jpeg'
+import image1 from '../img/Design.jpeg'
+import image2 from '../img/Planning.jpeg'
+import person from '../img/Person.jpeg'
+
+function About() {
     return (
         <>
-            <h1>About</h1>
+            <div className="main">
+                <div className="row">
+                    <div className="section">
+                        <div className="left">
+                        </div>
+                        <div className="right">
+                            <div className="title">
+                                <p>About Us</p>
+                            </div>
+                            <div className="header">
+                                <h1>Branding & Design Agency</h1>
+                            </div>
+                            <div className="text">
+                                <p>
+                                    We are Procreate Branding, a seasoned brand design and
+                                    communication agency driving profitable growth for brands,
+                                    leveraging end-to-end solutions. Critical thinking, precise
+                                    strategies, brilliant design language and boundless …
+                                </p>
+                            </div>
+                            <div className="btn mb-5">Learn More</div>
+                        </div>
+                    </div>
+                    <div className="box-group mt-5">
+                        <div className="box1">
+                            <img src={image} alt="" />
+                            <h2>Analysis</h2>
+                            <div className="text1">
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+                                    commodi quidem exercitationem quam dolor vel nostrum quaerat
+                                    consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
+                                    aspernatur ullam ea. Ducimus, sit.
+                                </p>
+                                <div className="btn1">More</div>
+                            </div>
+                        </div>
+                        <div className="box2">
+                            <img src={image2} alt="" />
+                            <h2>Planning</h2>
+                            <div className="text1">
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+                                    commodi quidem exercitationem quam dolor vel nostrum quaerat
+                                    consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
+                                    aspernatur ullam ea. Ducimus, sit.
+                                </p>
+                                <div className="btn1">More</div>
+                            </div>
+                        </div>
+                        <div className="box3">
+                            <img src={image1} alt="" />
+                            <h2>Design</h2>
+                            <div className="text1">
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+                                    commodi quidem exercitationem quam dolor vel nostrum quaerat
+                                    consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
+                                    aspernatur ullam ea. Ducimus, sit.
+                                </p>
+                                <div className="btn1">More</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- myapp/src/Component/Home.jsx
+++ myapp/src/Component/Home.jsx
@@ -1,74 +1,59 @@
 import '../static/Home.css'
 
-function Home(){
-    return (
-        <>
-        <div className="main">
-        <div className="row">
-        <div className="section">
-          <div className="left"></div>
-          <div className="right">
-            <div className="title">
-              <p>About Us</p>
-            </div>
-            <div className="header">
-              <h1>Branding & Design Agency</h1>
-            </div>
-            <div className="text">
-              <p>
-                We are Procreate Branding, a seasoned brand design and
-                communication agency driving profitable growth for brands,
-                leveraging end-to-end solutions. Critical thinking, precise
-                strategies, brilliant design language and boundless …
-              </p>
-            </div>
-            <div className="btn">Learn More</div>
+function Home() {
+  return (
+    <>
+      <div className="main">
+
+        <div className="center">
+          <h1>Selling on the internet like a pro</h1>
+          <p>
+            We know how large objects will act, but things on a small scale just
+            do not act that way.
+          </p>
+          <div className="buttons">
+            <a href="">Get Quote Now</a>
+            <a href="">Learn More</a>
           </div>
         </div>
-        <div className="box-group">
-          <div className="box1">
-            <img src="./img-6/WhatsApp Image 2025-01-23 at 11.38.51 AM.jpeg" alt="" />
-            <h2>Analysis</h2>
-            <div className="text1">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
-                commodi quidem exercitationem quam dolor vel nostrum quaerat
-                consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
-                aspernatur ullam ea. Ducimus, sit.
-              </p>
-              <div className="btn1">More</div>
-            </div>
+        <div className="cards">
+          <div className="card">
+            <img
+              src="https://cdn3.iconfinder.com/data/icons/emoji-81/48/6-1024.png"
+              alt="image"
+            />
+            <h3>Training Courses</h3>
+            <p>
+              The gradual accumulation of information about atomic and small-scale
+              behavior...
+            </p>
           </div>
-          <div className="box2">
-            <img src="./img-6/WhatsApp Image 2025-01-23 at 11.38.52 AM (1).jpeg" alt="" />
-            <h2>Planning</h2>
-            <div className="text1">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
-                commodi quidem exercitationem quam dolor vel nostrum quaerat
-                consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
-                aspernatur ullam ea. Ducimus, sit.
-              </p>
-              <div className="btn1">More</div>
-            </div>
+          <div className="card">
+            <img
+              src="https://thumbs.dreamstime.com/b/green-double-checking-icon-double-tick-check-mark-flat-done-sticker-icon-isolated-white-accept-button-green-double-checking-261549345.jpg"
+              alt=""
+            />
+            <h3>2,769 Online Courses</h3>
+            <p>
+              The gradual accumulation of information about atomic and small-scale
+              behavior...
+            </p>
           </div>
-          <div className="box3">
-            <img src="./img-6/WhatsApp Image 2025-01-23 at 11.38.52 AM.jpeg" alt="" />
-            <h2>Design</h2>
-            <div className="text1">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
-                commodi quidem exercitationem quam dolor vel nostrum quaerat
-                consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
-                aspernatur ullam ea. Ducimus, sit.
-              </p>
-              <div className="btn1">More</div>
-            </div>
+          <div className="card">
+            <img
+              src="https://images.emojiterra.com/google/android-12l/512px/1f4b3.png"
+              alt=""
+            />
+            {/* <!-- <div className="card" style="background-color: #00a8cc;"> --> */}
+            <h3>Training Courses</h3>
+            <p>
+              The gradual accumulation of information about atomic and small-scale
+              behavior...
+            </p>
           </div>
         </div>
       </div>
-    </div>
-        </>
-    )
+    </>
+  )
 }
 export default Home;
