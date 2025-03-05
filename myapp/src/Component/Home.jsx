f4537309500d9d7dbd539332ba826b9f0a2af871
jensi2512 <jensipatel19@gnu.ac.in>
2025-03-05 12:40:52 +0530

    update

--- myapp/src/Component/Home.jsx
+++ myapp/src/Component/Home.jsx
@@ -1,7 +1,73 @@
+import '../static/Home.css'
+
 function Home(){
     return (
         <>
-        <h1>home</h1>
+        <div className="main">
+        <div className="row">
+        <div className="section">
+          <div className="left"></div>
+          <div className="right">
+            <div className="title">
+              <p>About Us</p>
+            </div>
+            <div className="header">
+              <h1>Branding & Design Agency</h1>
+            </div>
+            <div className="text">
+              <p>
+                We are Procreate Branding, a seasoned brand design and
+                communication agency driving profitable growth for brands,
+                leveraging end-to-end solutions. Critical thinking, precise
+                strategies, brilliant design language and boundless …
+              </p>
+            </div>
+            <div className="btn">Learn More</div>
+          </div>
+        </div>
+        <div className="box-group">
+          <div className="box1">
+            <img src="./img-6/WhatsApp Image 2025-01-23 at 11.38.51 AM.jpeg" alt="" />
+            <h2>Analysis</h2>
+            <div className="text1">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+                commodi quidem exercitationem quam dolor vel nostrum quaerat
+                consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
+                aspernatur ullam ea. Ducimus, sit.
+              </p>
+              <div className="btn1">More</div>
+            </div>
+          </div>
+          <div className="box2">
+            <img src="./img-6/WhatsApp Image 2025-01-23 at 11.38.52 AM (1).jpeg" alt="" />
+            <h2>Planning</h2>
+            <div className="text1">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+                commodi quidem exercitationem quam dolor vel nostrum quaerat
+                consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
+                aspernatur ullam ea. Ducimus, sit.
+              </p>
+              <div className="btn1">More</div>
+            </div>
+          </div>
+          <div className="box3">
+            <img src="./img-6/WhatsApp Image 2025-01-23 at 11.38.52 AM.jpeg" alt="" />
+            <h2>Design</h2>
+            <div className="text1">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+                commodi quidem exercitationem quam dolor vel nostrum quaerat
+                consequatur fuga omnis nam quibusdam numquam, blanditiis culpa
+                aspernatur ullam ea. Ducimus, sit.
+              </p>
+              <div className="btn1">More</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
         </>
     )
 }
