c487e4b9da0d6d0418fc59cb018310faacb54511
jensi2512 <jensipatel19@gnu.ac.in>
2025-03-07 19:02:51 +0530

    form

--- my-app3/src/Component/Home.jsx
+++ my-app3/src/Component/Home.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 
 function Home() {
-    const [firstname, setFirstname] = useState("")
-    const [secondname, setSecondname] = useState("")
+  const [firstname, setFirstname] = useState("")
+  const [secondname, setSecondname] = useState("")
   const [email, setEmail] = useState("")
   const [phonenumber, setPhonenumber] = useState("")
   const [address, setAddress] = useState("")
   const [data, setData] = useState([])
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault()
-    setData((preval) => [...preval, {firstname, secondname, email, phonenumber}] )
+    setData((preval) => [...preval, { firstname, secondname, email, phonenumber }])
     setEmail("")
     setSecondname("")
     setFirstname("")
@@ -18,22 +18,22 @@ function Home() {
     setAddress("")
   }
 
-  function handleFirstname(e){
-     setFirstname(e.target.value)
+  function handleFirstname(e) {
+    setFirstname(e.target.value)
   }
-  function handleSecondname(e){
-     setSecondname(e.target.value)
+  function handleSecondname(e) {
+    setSecondname(e.target.value)
   }
 
-  function handleEmail(e){
+  function handleEmail(e) {
     setEmail(e.target.value)
   }
 
-  function handlePhonenumber(e){
+  function handlePhonenumber(e) {
     setPhonenumber(e.target.value)
   }
 
-  function handleAddress(e){
+  function handleAddress(e) {
     setAddress(e.target.value)
   }
 
@@ -44,19 +44,19 @@ function Home() {
           <div className="col-5 m-auto">
             <form action="" onSubmit={handleSubmit}>
               <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Firstname" aria-label="Firstname" aria-describedby="basic-addon1" value={firstname}  onChange={handleFirstname}/>
+                <input type="text" className="form-control" placeholder="Firstname" aria-label="Firstname" aria-describedby="basic-addon1" value={firstname} onChange={handleFirstname} />
               </div>
               <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Secondname" aria-label="Secondname" aria-describedby="basic-addon1" value={secondname}  onChange={handleSecondname}/>
+                <input type="text" className="form-control" placeholder="Secondname" aria-label="Secondname" aria-describedby="basic-addon1" value={secondname} onChange={handleSecondname} />
               </div>
               <div className="input-group mb-3">
-                <input type="email" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"value={email} onChange={handleEmail}/>
+                <input type="email" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" value={email} onChange={handleEmail} />
               </div>
               <div className="input-group mb-3">
-                <input type="number" className="form-control" placeholder="Phonenumber" aria-label="Phonenumber" aria-describedby="basic-addon1"value={phonenumber} onChange={handlePhonenumber}/>
+                <input type="number" className="form-control" placeholder="Phonenumber" aria-label="Phonenumber" aria-describedby="basic-addon1" value={phonenumber} onChange={handlePhonenumber} />
               </div>
               <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Address" aria-label="Address" aria-describedby="basic-addon1" value={address}  onChange={handleAddress}/>
+                <input type="text" className="form-control" placeholder="Address" aria-label="Address" aria-describedby="basic-addon1" value={address} onChange={handleAddress} />
               </div>
               <button type="submit" className="btn btn-success">Submit</button>
             </form>
@@ -65,30 +65,30 @@ function Home() {
 
         <hr />
         <div className="row">
-           <div className="col-5 m-auto">
-               <table className="table">
-                    <thead>
-                        <tr>
-                           <th>Firstname</th>
-                           <th>Secondname</th>
-                           <th>Email</th>
-                           <th>Phonenumber</th>
-                           <th>Address</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                       {data.length > 0 && data.map((ele, index) => (
-                          <tr key={index}>
-                              <td>{ele.firstname}</td>
-                              <td>{ele.secondname}</td>
-                              <td>{ele.email}</td>
-                              <td>{ele.phonenumber}</td>
-                              <td>{ele.address}</td>
-                          </tr>
-                       ))}
-                    </tbody>
-               </table>
-           </div>
+          <div className="col-5 m-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Firstname</th>
+                  <th>Secondname</th>
+                  <th>Email</th>
+                  <th>Phonenumber</th>
+                  <th>Address</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.length > 0 && data.map((ele, index) => (
+                  <tr key={index}>
+                    <td>{ele.firstname}</td>
+                    <td>{ele.secondname}</td>
+                    <td>{ele.email}</td>
+                    <td>{ele.phonenumber}</td>
+                    <td>{ele.address}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
