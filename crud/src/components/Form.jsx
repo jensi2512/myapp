a248836dc7ffc8e357d1d9c07debcf1f8bb654d6
jensi2512 <jensipatel19@gnu.ac.in>
2025-03-12 10:16:10 +0530

    add and toast

--- crud/src/components/Form.jsx
+++ crud/src/components/Form.jsx
@@ -1,31 +1,47 @@
 import { useNavigate } from "react-router-dom"
+import { postData } from "../server"
+import { useState } from "react"
+import { toast } from 'react-toastify';
 
 function Form() {
 
     const navigate = useNavigate()
+    const [formData, setFormData] = useState({username : "", email : "", mobile : "", address : ""})
+
+    function handleChange(e){
+        setFormData(preval => ({...preval, [e.target.name] : e.target.value}))
+    }
+
+   async function handleSubmit(e){
+        e.preventDefault()
+        await postData(formData)
+        toast.success("User add Successfully")
+        setFormData({username : "", email : "", mobile : "", address : ""})
+        navigate('/')
+    }
 
     return (
         <div>
             <div className="container mt-5">
                 <div className="row">
                     <div className="col-5 m-auto">
-                        
-                        <form action="">
+
+                        <form action="" onSubmit={handleSubmit}>
                             <div className="input-group mb-3">
-                                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" name="username" value={formData.username} onChange={handleChange} required/>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="email" className="form-control" placeholder="Email" aria-label="email" aria-describedby="basic-addon1" />
+                                <input type="email" className="form-control" placeholder="Email" aria-label="email" aria-describedby="basic-addon1" name="email" value={formData.email} onChange={handleChange} required/>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="number" className="form-control" placeholder="Mobile" aria-label="Mobile" aria-describedby="basic-addon1" />
+                                <input type="tel" className="form-control" placeholder="Mobile" aria-label="Mobile" aria-describedby="basic-addon1" name="mobile" value={formData.mobile} onChange={handleChange} required/>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="text" className="form-control" placeholder="Address" aria-label="Address" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Address" aria-label="address" aria-describedby="basic-addon1" name="address" value={formData.address} onChange={handleChange} required/>
                             </div>
-                        </form>
-                        <button className="btn btn-success">Submit</button>
+                        <button className="btn btn-success" type="submit">Submit</button>
                         <button className="btn btn-danger ms-3" onClick={() => navigate('/')}>Close</button>
+                        </form>
                     </div>
                 </div>
             </div>
--- crud/src/components/Table.jsx
+++ crud/src/components/Table.jsx
@@ -2,52 +2,60 @@ import { useNavigate } from "react-router-dom"
 import { getData } from "../server"
 import { useState, useEffect } from "react"
 function Table() {
-    const navigate = useNavigate()
-    const [data, setData] = useState([])
+  const navigate = useNavigate()
+  const [data, setData] = useState([])
 
 
-    useEffect(() => {
-        async function fetchData() {
-            const response = await getData()
-            setData(response)
-        }
-        fetchData()
-    }, [])
+  useEffect(() => {
+    async function fetchData(){
+      const response  = await getData()
+      setData(response)
+    }
+    fetchData()
+  }, [])
 
-    return (
-        <div>
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-5 m-auto">
-                        <button className="btn btn-outline-warning" onClick={() => navigate('/form')}>Add User</button>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Username</th>
-                                    <th>Email</th>
-                                    <th>Mobile</th>
-                                    <th>Address</th>
-                                    <th>Action</th>
+
+
+  return (
+    <div>
+        <div className="container mt-5">
+            <div className="row">
+               <div className="col-5 m-auto">
+               <button className="btn btn-outline-warning" onClick={() => navigate('/form')}>Add User</button>
+                   <table className="table">
+                      <thead>
+                         <tr>
+                           <th>No.</th>
+                           <th>Username</th>
+                           <th>Email</th>
+                           <th>Mobile</th>
+                           <th>Address</th>
+                           <th>Action</th>
+                         </tr>
+                      </thead>
+                        <tbody>
+                            {data.length > 0 && data.map((element, index) => {
+                              return (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{element.username}</td>
+                                    <td>{element.email}</td>
+                                    <td>{element.mobile}</td>
+                                    <td>{element.address}</td>
+                                    <td>
+                                      <button className="btn"><i className="bi bi-pencil-square text-primary fs-5"></i></button>
+                                      <button className="btn"><i className="bi bi-trash3 text-danger fs-5"></i></button>
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {data.length > 0 && data.map((element, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <td>{element.username}</td>
-                                            <td>{element.email}</td>
-                                            <td>{element.mobile}</td>
-                                            <td>{element.address}</td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                              )
+                            })}
+                        </tbody>
+                   </table>
+               </div>
             </div>
         </div>
-    )
+    </div>
+  )
 }
 
 export default Table
